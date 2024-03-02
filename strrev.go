4664bda38b7c92d401e2ddf3b0a9cb099cd2634f
Stephen Kisengese <skisenge@noreply.learn.zone01kisumu.ke>
2024-03-02 04:49:03 +0000

    Update 'strrev.go'

--- strrev.go
+++ strrev.go
@@ -4,8 +4,8 @@ import "github.com/01-edu/z01"
 
 func StrRev(s string) string {
 	Astr := []rune(s)
-	for r, _ := range Astr {
-		defer z01.PrintRune(Astr[r])
+	for i := len(Astr) - 1; i >= 0; i-- {
+		z01.PrintRune(Astr[r])
 	}
 	return string('\n')
 }
