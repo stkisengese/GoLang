dc605cdf387f6a1771af09d9a17ba8f5bb6d09fd
skisenge <skisengese@gmail.com>
2024-03-02 14:23:08 +0300

    strrev

--- strrev.go
+++ strrev.go
@@ -4,7 +4,7 @@ func StrRev(s string) string {
 	var result string /* empty string to store the reversed result */
 
 	for i := len(s) - 1; i >= 0; i-- {
-		result += string(s[i]) //append to result string
+		result += string(s[i]) // append to result string
 	}
 	return result
 }
