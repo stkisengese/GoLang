7a8784f1a0ab708937cd6e0609803ffe40e51a8b
Stephen Kisengese <skisenge@noreply.learn.zone01kisumu.ke>
2024-03-02 05:44:51 +0000

    Update 'strrev.go'

--- strrev.go
+++ strrev.go
@@ -1,12 +1,11 @@
 package piscine
 
-
 func StrRev(s string) string {
 	Astr := []rune(s)
-	var revRun []rune
+	var revRun[]rune
 
 	for i := len(Astr) - 1; i >= 0; i-- {
-		revRun = append(revRun, (Astr[i]))
-	}
+		revRun = append(revRun, (Astr[i])) 	
+		}
 	return string(revRun)
 }
