56fd95264881b55f9c59f14675371b5691782958
Stephen Kisengese <skisenge@noreply.learn.zone01kisumu.ke>
2024-03-02 05:16:38 +0000

    Update 'strrev.go'

--- strrev.go
+++ strrev.go
@@ -1,11 +1,12 @@
 package piscine
 
-import "github.com/01-edu/z01"
 
 func StrRev(s string) string {
 	Astr := []rune(s)
+	var revRun [] rune
+
 	for i := len(Astr) - 1; i >= 0; i-- {
-		z01.PrintRune(Astr[r])
+		revRun = append(revRun, (Astr[i]))
 	}
-	return string('\n')
+	return string(revRun)
 }
