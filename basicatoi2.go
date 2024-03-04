1fbc1fcbedb2436bc946a2753dcd55dd30bbedb8
skisenge <skisengese@gmail.com>
2024-03-04 14:51:22 +0300

    recursive!

--- basicatoi2.go
+++ basicatoi2.go
@@ -4,7 +4,7 @@ func BasicAtoi2(s string) int {
 	result := 0 // initializing an integer 0
 	for _, c := range s {
 		if c >= '0' && c <= '9' {
-			digit := int(c - '0')
+			digit := int(c - 0)
 			result = result*10 + digit
 		} else {
 			return 0
