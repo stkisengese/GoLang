49d20ff5ac9cf175382a26b1dc92370ff0eebc7d
skisenge <skisengese@gmail.com>
2024-03-04 19:19:23 +0300

    fibonacci

---
+++ fibonacci.go
@@ -0,0 +1,12 @@
+package piscine
+
+func Fibonacci(index int) int {
+	if index == 0 {
+		return 0
+	}
+	if index < 0 {
+		return -1
+	} else {
+		return Fibonacci(index-1) + Fibonacci(index-2)
+	}
+}
