2d7b526a8753a2a02348825ca529569053fa9b06
Christopher Fremond <dev@01-edu.org>
2024-03-05 19:33:45 +0000

    Update 'printcomb.go'

--- printcomb.go
+++ printcomb.go
@@ -6,9 +6,9 @@ func PrintComb() {
   for i := 0; i <= 7; i++ {
     for j := i+1; j <= 8; j++ {
       for k := j+1; k <= 9; k++ {
-        z01.PrintRune(rune(i + '0'))
-        z01.PrintRune(rune(j + '0'))
-        z01.PrintRune(rune(k + '0'))
+        z01.PrintRune('i')
+        z01.PrintRune('j')
+        z01.PrintRune('k')
         if i != 7 || j != 8 || k != 9 {
            z01.PrintRune(',') 
            z01.PrintRune(' ')
