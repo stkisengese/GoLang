bbf243399ff2a6f19a87e2fc8d1577d44ef0b1e2
skisenge <skisengese@gmail.com>
2024-03-05 19:20:06 +0300

    findprime

--- findnextprime.go
+++ findnextprime.go
@@ -4,15 +4,16 @@ func FindNextPrime(nb int) int {
 	if nb <= 1 {
 		return 2
 	}
-	prime := 1
-	for i := 2; i < nb; i++ {
-		if (nb % i) == 0 {
-			nb++
+	if IsPrime(nb) {
+		return nb
+	}
+	var i int = nb + 1
+	for nb < i {
+		if IsPrime(i) {
+			return i
+		} else {
+			i++
 		}
 	}
-	if prime == 1 {
-		return nb
-	} else {
-		return nb
-	}
+	return 0
 }
