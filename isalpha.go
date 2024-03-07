cbaae1d3775f8d7a3f4685e105a3ee382f87509a
skisenge <skisengese@gmail.com>
2024-03-07 04:46:39 +0300

    isalpha

--- isalpha.go
+++ isalpha.go
@@ -3,8 +3,10 @@ package piscine
 func IsAlpha(s string) bool {
 	for _, r := range s {
 		if r >= 'a' && r <= 'z' || r >= 'A' && r <= 'Z' || r >= '0' && r <= '9' {
-			return true
+			continue
+		} else {
+			return false
 		}
 	}
-	return false
+	return true
 }
