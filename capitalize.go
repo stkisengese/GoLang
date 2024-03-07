7d49d099ddbf7b090a12c0f98ac67f42336d5abe
Christopher Fremond <dev@01-edu.org>
2024-03-07 18:40:13 +0000

    Add 'capitalize.go'

---
+++ capitalize.go
@@ -0,0 +1,29 @@
+package piscine
+
+func Capitalize(s string) string {
+	var capital string
+	measure := true
+	for _, r := range s {
+		if r >= 'a' && r <= 'z' || r >= 'A' && r <= 'Z' || r >= '0' && r <= '9' {
+			if measure {
+				if r >= 'a' && r <= 'z' {
+					capital += string(r - 32)
+				} else {
+					capital += string(r)
+				}
+				measure = false
+			} else {
+				if r >= 'A' && r <= 'Z' {
+					capital += string(r + 32)
+				} else {
+					capital += string(r)
+				}
+			}
+
+		} else {
+			capital += string(r)
+			measure = true
+		}
+	}
+	return capital
+}
