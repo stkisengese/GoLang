b7077783435a9e53e922a735652bf12e068fe5dd
skisenge <skisengese@gmail.com>
2024-03-07 05:08:51 +0300

    isprint

---
+++ isprintable.go
@@ -0,0 +1,12 @@
+package piscine
+
+func IsPrintable(s string) bool {
+	for _, r := range s {
+		if r >= 'a' && r <= 'z' || r >= 'A' && r <= 'Z' {
+			continue
+		} else {
+			return false
+		}
+	}
+	return true
+}
