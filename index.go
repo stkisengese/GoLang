83172228859d7ba2066912e3db5dc88170d79f2c
skisenge <skisengese@gmail.com>
2024-03-07 03:01:51 +0300

    index

---
+++ index.go
@@ -0,0 +1,13 @@
+package piscine
+
+func Index(s string, toFind string) int {
+	if len(toFind) == 0 {
+		return 0
+	}
+	for i := 0; i < len(s)-len(toFind); i++ {
+		if s[i:i+len(toFind)] == toFind {
+			return i
+		}
+	}
+	return -1
+}
