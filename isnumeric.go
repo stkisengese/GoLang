a13fe9c49ec955a5f98fdb86392534eb0d1d3be8
skisenge <skisengese@gmail.com>
2024-03-07 04:55:59 +0300

    inumeric

--- isnumeric.go
+++ isnumeric.go
@@ -3,8 +3,10 @@ package piscine
 func IsNumeric(s string) bool {
 	for _, r := range s {
 		if r >= '0' && r <= '9' {
-			return true
+			continue
+		} else {
+			return false
 		}
 	}
-	return false
+	return true
 }
