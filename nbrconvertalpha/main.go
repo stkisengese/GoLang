2272581dd6a727a834101fdebde5d163bcd086f0
skisenge <skisengese@gmail.com>
2024-03-08 23:07:46 +0300

    nbrparams

---
+++ nbrconvertalpha/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"os"
+
+	"github.com/01-edu/z01"
+)
+
+func main() {
+	args := os.Args
+	uppercase := false
+	if len(args) > 0 && args[0] == "--upper" {
+		uppercase = true
+		args = args[1:]
+	}
+	if len(args) == 0 {
+		return
+	}
+	for _, arg := range args {
+		num := 0
+		for _, ch := range arg {
+			if ch < '0' {
+				z01.PrintRune(' ')
+				continue
+			}
+			num = num*10 + int(ch-'0')
+		}
+		if num >= 1 && num <= 26 {
+			if uppercase {
+				z01.PrintRune('A' + rune(num) - 1)
+			} else {
+				z01.PrintRune('a' + rune(num) - 1)
+			}
+		} else {
+			z01.PrintRune(' ')
+		}
+	}
+	z01.PrintRune('\n')
+}
