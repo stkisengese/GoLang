7060c5cf211cb0a90c959a324c78bb7fb7c894ed
skisenge <skisengese@gmail.com>
2024-03-09 16:15:03 +0300

    splitspaces

---
+++ splitwhitespaces.go
@@ -0,0 +1,22 @@
+package piscine
+
+func isWhiteSpace(r rune) bool {
+	return r == ' ' || r == '\t' || r == '\n'
+}
+
+func SplitWhitespaces(s string) []string {
+	var words []string
+	start := 0
+	for i, r := range s {
+		if isWhiteSpace(r) {
+			if i > start {
+				words = append(words, s[start:i])
+			}
+			start = i + 1
+		}
+		if start < len(s) {
+			words = append(words, s[start:])
+		}
+	}
+	return words
+}
