acbcb85bc1315030d797ee22989341c56ba860d6
skisenge <skisengese@gmail.com>
2024-03-09 12:55:42 +0300

    appendrange

---
+++ appendrange.go
@@ -0,0 +1,12 @@
+package piscine
+
+func AppendRange(min, max int) []int {
+	if min >= max {
+		return nil
+	}
+	var answer [] int
+	for i := min; i < max; i++ {
+		answer = append(answer, i)
+	}
+	return answer
+}
