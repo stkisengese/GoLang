1647a2fd18038823fc894fcfad25bd632968e64c
skisenge <skisengese@gmail.com>
2024-03-12 15:37:38 +0300

    updates

--- split.go
+++ split.go
@@ -1,17 +1,17 @@
 package piscine
 
 func Split(s, sep string) []string {
-    result := [] string{}
-    subStr := 0
-    for i := 0; i < len(s); i++ {
-        if i + len(sep) <= len(s) && s[i:i+len(sep)] == sep {
-             result = append(result, s[subStr:i])
-              subStr = i+len(sep)
-              i += len(sep) - 1         
-        }
-    } 
-        if subStr < len(s) {
-            result = append(result, s[subStr:])
-        }
-    return result
+	result := []string{}
+	subStr := 0
+	for i := 0; i < len(s); i++ {
+		if i+len(sep) <= len(s) && s[i:i+len(sep)] == sep {
+			result = append(result, s[subStr:i])
+			subStr = i + len(sep)
+			i += len(sep) - 1
+		}
+	}
+	if subStr < len(s) {
+		result = append(result, s[subStr:])
+	}
+	return result
 }
