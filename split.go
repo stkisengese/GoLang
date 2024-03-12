25ec0e5cddfeaac41386386177bd10f90d9e048f
skisenge <skisengese@gmail.com>
2024-03-12 15:35:23 +0300

    updates

--- split.go
+++ split.go
@@ -6,12 +6,12 @@ func Split(s, sep string) []string {
     for i := 0; i < len(s); i++ {
         if i + len(sep) <= len(s) && s[i:i+len(sep)] == sep {
              result = append(result, s[subStr:i])
-        subStr = i+len(sep)
-          i += len(sep) - 1         
+              subStr = i+len(sep)
+              i += len(sep) - 1         
         }
-       } 
-          if subStr < len(s) {
+    } 
+        if subStr < len(s) {
             result = append(result, s[subStr:])
-          }
+        }
     return result
 }
