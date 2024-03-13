a98dab273f72dd428d51b3800e59415675ec0bf7
Stephen Kisengese <skisenge@noreply.learn.zone01kisumu.ke>
2024-03-13 08:39:34 +0000

    descendcomb

---
+++ descendcomb.go
@@ -0,0 +1,21 @@
+package piscine
+
+import "github.com/01-edu/z01"
+
+func DescendComb() {
+	for i := 99; i >= 0; i-- {
+		for j := i - 1; j >= 0; j-- {
+			z01.PrintRune(rune(i/10 + '0'))
+			z01.PrintRune(rune(i%10 + '0'))
+			z01.PrintRune(' ')
+			z01.PrintRune(rune(j/10 + '0'))
+			z01.PrintRune(rune(j%10 + '0'))
+
+			if i != 0 || j != 0 {
+				z01.PrintRune(',')
+				z01.PrintRune(' ')
+			}
+		}
+	}
+	z01.PrintRune('\n')
+}
