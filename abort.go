af51348548ff20533ccc86904b43c7ad2d31dd32
skisenge <skisengese@gmail.com>
2024-03-13 16:11:08 +0300

    f

--- abort.go
+++ abort.go
@@ -2,12 +2,13 @@ package piscine
 
 func Abort(a, b, c, d, e int) int {
 	number := []int{a, b, c, d, e}
-
-	for j := 0; j < len(number)-1; j++ {
-		if number[j] > number[j+1] {
-			f := number[j]
-			number[j] = number[j+1]
-			number[j+1] = f
+	for i := 0; i < len(number)-1; i++ {
+		for j := 0; j < len(number)-1; j++ {
+			if number[j] > number[j+1] {
+				f := number[j]
+				number[j] = number[j+1]
+				number[j+1] = f
+			}
 		}
 	}
 	return number[2]
