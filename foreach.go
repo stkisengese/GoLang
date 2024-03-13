917135c62b8def443b8501d0ff0cd8891f8cf0a2
skisenge <skisengese@gmail.com>
2024-03-13 15:59:52 +0300

    f

--- foreach.go
+++ foreach.go
@@ -1,14 +1,7 @@
 package piscine
 
-import "github.com/01-edu/z01"
-
 func ForEach(f func(int), a []int) {
-	for _, element := range a {
-		f(element)
+	for {
+		f(a[i])
 	}
-}
-
-func PrintNbr(n int) {
-	digitRune := rune(n + '0')
-	z01.PrintRune(digitRune)
 }
--- test/main.go
+++ test/main.go
@@ -1,8 +1,13 @@
 package main
 
-import "piscine"
+import (
+	"fmt"
+	"piscine"
+)
 
 func main() {
-	a := piscine.SplitWhiteSpaces("Hello how are you?")
-	piscine.PrintWordsTables(a)
+	fmt.Println(piscine.FoodDeliveryTime("burger"))
+	fmt.Println(piscine.FoodDeliveryTime("chips"))
+	fmt.Println(piscine.FoodDeliveryTime("nuggets"))
+	fmt.Println(piscine.FoodDeliveryTime("burger") + piscine.FoodDeliveryTime("chips") + piscine.FoodDeliveryTime("nuggets"))
 }
