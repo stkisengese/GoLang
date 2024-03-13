e30a73b179401ca2bdb15ba3795003e1554e6693
skisenge <skisengese@gmail.com>
2024-03-13 13:28:17 +0300

    food

---
+++ fooddeliverytime.go
@@ -0,0 +1,20 @@
+package piscine
+
+type food struct {
+	preptime int
+}
+
+func FoodDeliveryTime(order string) int {
+	var preptime int
+	switch order {
+	case "burger":
+		preptime = 15
+	case "chips":
+		preptime = 10
+	case "nuggets":
+		preptime = 12
+	default:
+		preptime = 37
+	}
+	return preptime
+}
