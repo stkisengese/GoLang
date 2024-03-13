38661338abbddf5245c7c9c77dc04c2cfa3767e6
Stephen Kisengese <skisenge@noreply.learn.zone01kisumu.ke>
2024-03-13 09:11:39 +0000

    Update 'rot14.go'

--- rot14.go
+++ rot14.go
@@ -1,16 +1,16 @@
 package piscine
 
 func Rot14(s string) string {
-    var result string
-    for _, r := range s {
-            switch {
-            case r >= 'a' && r <= 'z':
-                result += string((r - 'a' + 14)%26 + 'a')
-            case r >= 'A' && r <= 'Z': 
-                result += string((r - 'A' + 14)%26 + 'A') 
-            default:
-                result += string(r)
-            }
-    }
-  return result
+        var result string
+        for _, r := range s {
+                switch {
+                case r >= 'a' && r <= 'z':
+                        result += string((r-'a'+14)%26 + 'a')
+                case r >= 'A' && r <= 'Z': 
+                        result += string((r-'A'+14)%26 + 'A') 
+                default:
+                        result += string(r)
+                }
+        }
+        return result
 }
