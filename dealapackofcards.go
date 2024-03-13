01f9ad97b1ef4e49b1a2be555fcc745e68ebfb0f
skisenge <skisengese@gmail.com>
2024-03-13 19:00:43 +0300

    deck

--- dealapackofcards.go
+++ dealapackofcards.go
@@ -1,16 +1,22 @@
 package piscine
 
-import "fmt"
+import "github.com/01-edu/z01"
 
 func DealAPackOfCards(deck []int) {
 	players := 4
-	cards := 12
+	cards := len(deck) / 4
 	for i := 0; i < players; i++ {
 		start := i * cards
 		end := (i + 1) * cards
-
+		if i == players-1 {
+			end = len(deck)
+		}
 		playerCards := deck[start:end]
-
-		fmt.Printf("Player %d: %v\n", players, playerCards)
+		z01.PrintRune("Player ")
+		z01.PrintRune(i + 1)
+		z01.PrintRune(':')
+		z01.PrintRune(' ')
+		z01.PrintRune(playerCards)
+		z01.PrintRune('\n')
 	}
 }
