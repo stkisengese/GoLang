a080380f27e00aa740ce4029f7ef76efba46baaa
skisenge <skisengese@gmail.com>
2024-03-14 03:44:55 +0300

    podium

---
+++ podiumposition.go
@@ -0,0 +1,17 @@
+package piscine
+
+func PodiumPosition(podium [][]string) [][]string {
+	for i := range podium {
+		for j := 0; j < len(podium); j++ {
+			for k := j + 1; k < len(podium[i]); k++ {
+				if podium[i][j][0] < podium[i][k][0] {
+					swap := podium[i][j]
+					podium[i][j] = podium[i][k]
+					podium[i][k] = swap
+				}
+			}
+		}
+
+	}
+	return podium
+}
