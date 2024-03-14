49c5f0e09d9fadc6ccee8f30dc4b930994df4f35
skisenge <skisengese@gmail.com>
2024-03-14 09:36:32 +0300

    bread

---
+++ loafofbread.go
@@ -0,0 +1,25 @@
+package piscine
+
+func LoafOfBread(str string) string {
+	result := ""
+	i := 0
+	for i < len(str) {
+		if i+5 > len(str) {
+			return "Invalid Output\n"
+		}
+		substring := ""
+		for j := 0; j < 5; j++ {
+			if str[i+j] != ' ' {
+				substring += string(str[i+j])
+			} else {
+				j--
+			}
+		}
+		result += substring + "\n"
+		i += 6
+	}
+	if len(result) > 0 && result[len(result)-1] == '\n' {
+		result = result[:len(result)-1] // slice to remove the last character
+	}
+	return result
+}
