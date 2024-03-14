a04b3f808d8960c260dd8f23844a6223065c4774
skisenge <skisengese@gmail.com>
2024-03-14 09:06:57 +0300

    issorted

--- issorted.go
+++ issorted.go
@@ -1,24 +1,20 @@
 package piscine
 
 func IsSorted(f func(a, b int) int, a []int) bool {
-	if len(a) <= 1 {
-		return true
-	}
-	for i := 0; i < len(a)-1; i++ {
+	sorted := true
+	length := len(a)
+	for i := 0; i < length-1; i++ {
 		if f(a[i], a[i+1]) > 0 {
-			return false
+			sorted = false
 		}
 	}
-	return true
-}
-
-func isAscending(a, b int) int {
-	return abs(a) - abs(b)
-}
-
-func abs(x int) int { // to handle negative values
-	if x < 0 {
-		return -x
+	if !sorted {
+		sorted = true
+		for i := 0; i < length-1; i++ {
+			if f(a[i], a[i+1]) < 0 {
+				sorted = false
+			}
+		}
 	}
-	return x
+	return sorted
 }
