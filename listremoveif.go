4b3412b35c8c848c3662986ff0391defd3c44358
Christopher Fremond <dev@01-edu.org>
2024-03-19 17:39:49 +0000

    Add 'listremoveif.go'

---
+++ listremoveif.go
@@ -0,0 +1,33 @@
+package piscine
+
+type NodeL struct {
+	Data interface{}
+	Next *NodeL
+}
+
+type List struct {
+	Head *NodeL
+	Tail *NodeL
+}
+
+func ListRemoveIf(l *List, data_ref interface{}) {
+    var prev *NodeL
+    newNode := l.Head
+    for newNode != nil {
+        if newNode.Data == data_ref {
+            if prev == nil {
+                l.Head = newNode.Next
+            } else {
+                prev.Next = newNode.Next
+                if newNode == l.Tail {
+                    l.Tail = prev
+                }
+            }
+            newNode = newNode.Next
+        } else {
+          prev = newNode
+          newNode = newNode.Next
+        }
+    }
+}
+
