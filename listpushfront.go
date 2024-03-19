1f5fe282073b7fcba7f64b78293bd733734ef75e
skisenge <skisengese@gmail.com>
2024-03-19 12:48:36 +0300

    nodel

--- listpushfront.go
+++ listpushfront.go
@@ -1,13 +1,13 @@
 package piscine
 
-func ListPushBack(l *List, data interface{}) {
+func ListPushFront(l *List, data interface{}) {
 	newNode := &NodeL{Data: data}
 	if l.Head == nil {
 		l.Head = newNode
 		l.Tail = newNode
 		return
 	} else {
-		l.Tail.Next = newNode
-		l.Tail = newNode
+		newNode.Next = l.Head
+		l.Head = newNode
 	}
 }
