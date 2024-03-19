1f8ecb2cc9a75067608929a4de6d34241786fe2a
skisenge <skisengese@gmail.com>
2024-03-19 19:37:35 +0300

    listfind

--- listfind.go
+++ listfind.go
@@ -6,10 +6,10 @@ func CompStr(a, b interface{}) bool {
 
 func ListFind(l *List, ref interface{}, comp func(a, b interface{}) bool) *interface{} {
 	newHead := l.Head    // start from head list
-	for newHead != nil { //iterate thro the list
+	for newHead != nil { // iterate thro the list
 		if comp(newHead.Data, ref) { // compare data in the current mode
 			return &newHead.Data // pointer to the data in the newHead mode
-		} //returns when data matches
+		} // returns when data matches
 		newHead = newHead.Next // move to the next node
 	}
 	return nil
