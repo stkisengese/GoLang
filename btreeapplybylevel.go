576748b0de4d4b1d5417bc4a8c0d9f1d3efc863c
skisenge <skisengese@gmail.com>
2024-03-21 18:11:35 +0300

    bylevel

---
+++ btreeapplybylevel.go
@@ -0,0 +1,21 @@
+package piscine
+
+func BTreeApplyByLevel(root *TreeNode, f func(...interface{}) (int, error)) {
+	if root == nil {
+		return
+	}
+	queue := []*TreeNode{root}
+	for len(queue) > 0 {
+		levelSize := len(queue)
+		for i := 0; i < levelSize; i++ {
+			currentNode := queue[0]
+			queue = queue[1:]
+			if currentNode.Left != nil {
+				queue = append(queue, currentNode.Left)
+			}
+			if currentNode.Right != nil {
+				queue = append(queue, currentNode.Right)
+			}
+		}
+	}
+}
