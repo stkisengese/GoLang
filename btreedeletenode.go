163fce447264064a665ee662bbc2d4e43175bda6
skisenge <skisengese@gmail.com>
2024-03-21 19:48:17 +0300

    BTree

--- btreedeletenode.go
+++ btreedeletenode.go
@@ -52,6 +52,22 @@ func BTreeMin(root *TreeNode) *TreeNode {
 	return currentNode
 }
 
+func BTreeIsBinary(root *TreeNode) bool {
+	if root == nil {
+		return true
+	}
+	if root.Left != nil && root.Left.Data > root.Data {
+		return false
+	}
+	if root.Right != nil && root.Right.Data < root.Data {
+		return false
+	}
+	if !BTreeIsBinary(root.Left) || !BTreeIsBinary(root.Right) {
+		return false
+	}
+	return true
+}
+
 func BTreeDeleteNode(root, node *TreeNode) *TreeNode {
 	if root == nil {
 		return nil
