72996f1b5614a39299e3bce35f72505ca7606099
skisenge <skisengese@gmail.com>
2024-03-21 12:35:04 +0300

    isert

--- printcomb2.go
+++ printcomb2.go
@@ -3,18 +3,31 @@ package piscine
 import "github.com/01-edu/z01"
 
 func PrintComb2() {
-	for i := 0; i <= 98; i++ {
-		for j := i + 1; j <= 99; j++ {
-			z01.PrintRune(rune(i/10 + '0'))
-			z01.PrintRune(rune(i%10 + '0'))
-			z01.PrintRune(' ')
-			z01.PrintRune(rune(j/10 + '0'))
-			z01.PrintRune(rune(j%10 + '0'))
 
-			if i != 98 || j != 99 {
-				z01.PrintRune(',')
-				z01.PrintRune(' ')
+	for a := '0'; a <= '9'; a++ {
+		for b := '0'; b <= '9'; b++ {
+			for c := '0'; c <= '9'; c++ {
+				for d := '0'; d <= '9'; d++ {
+					if a == '9' && b == '8' && c == '9' && d == '9' {
+						z01.PrintRune(a)
+						z01.PrintRune(b)
+						z01.PrintRune(32)
+						z01.PrintRune(c)
+						z01.PrintRune(d)
+					} else if a == c && b < d || a < c {
+						z01.PrintRune(a)
+						z01.PrintRune(b)
+						z01.PrintRune(32)
+						z01.PrintRune(c)
+						z01.PrintRune(d)
+						z01.PrintRune(44)
+						z01.PrintRune(32)
+
+					}
+
+				}
 			}
 		}
 	}
+	z01.PrintRune('\n')
 }
--- test/main.go
+++ test/main.go
@@ -6,11 +6,33 @@ import (
 )
 
 func main() {
-	link := &piscine.List{}
-	link2 := &piscine.List{}
-	piscine.ListPushBack(link, "three")
-	piscine.ListPushBack(link, 3)
-	piscine.ListPushBack(link, "1")
-	fmt.Println(piscine.ListLast(link))
-	fmt.Println(piscine.ListLast(link2))
+	root := &piscine.TreeNode{Data: "4"}
+	piscine.BTreeInsertData(root, "1")
+	piscine.BTreeInsertData(root, "7")
+	piscine.BTreeInsertData(root, "5")
+	selected := piscine.BTreeSearchItem(root, "7")
+	fmt.Print("Item selected -> ")
+	if selected != nil {
+		fmt.Println(selected.Data)
+	} else {
+		fmt.Println("nil")
+	}
+	fmt.Print("Parent of selected item -> ")
+	if selected.Parent != nil {
+		fmt.Println(selected.Parent.Data)
+	} else {
+		fmt.Println("nil")
+	}
+	fmt.Print("Left child of selected item -> ")
+	if selected.Left != nil {
+		fmt.Println(selected.Left.Data)
+	} else {
+		fmt.Println("nil")
+	}
+	fmt.Print("Right child of selected item -> ")
+	if selected.Right != nil {
+		fmt.Println(selected.Right.Data)
+	} else {
+		fmt.Println("nil")
+	}
 }
