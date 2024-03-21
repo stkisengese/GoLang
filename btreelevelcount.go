350dbf42a0a01494d4f8266f70aca659b6316f9e
skisenge <skisengese@gmail.com>
2024-03-21 16:50:56 +0300

    levelcount

--- btreelevelcount.go
+++ btreelevelcount.go
@@ -2,14 +2,14 @@ package piscine
 
 func BTreeLevelCount(root *TreeNode) int {
 	if root == nil {
-		return 0 // Empty tree has 0 levels
+		return 0
 	}
-	queue := []*TreeNode{root} // Queue to store nodes for level-order traversal
-	level := 0                 // Current level counter
-	for len(queue) > 0 {
+	queue := []*TreeNode{root} // to store level order traversal
+	level := 0
+	for queue != nil {
 		level++
 		for i := 0; i < len(queue); i++ {
-			node := queue[0]
+			node := queue[i]
 			if node.Left != nil {
 				queue = append(queue, node.Left)
 			}
@@ -17,7 +17,7 @@ func BTreeLevelCount(root *TreeNode) int {
 				queue = append(queue, node.Right)
 			}
 		}
-		queue = queue[len(queue):]
+		queue = queue[len(queue):] // remove processed nodes from th equeue
 	}
 	return level
 }
